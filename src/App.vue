61f4c10b92362ce72a18179c7a02eea35aa519dd
Caio Santos da Silva <vorreicaio@hotmail.com>
2022-07-12 16:48:21 -0500

    new commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+import NoteVue from "./components/Note.vue";
+</script>
+
+<template>
+  <div class="nav">
+    <div>
+      <h1>Note Online</h1>
+      
+    </div>
+  </div>
+  <div class="container">
+    <NoteVue />
+  </div>
+  <div class="footer">foot</div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+.nav {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: black;
+  height: 150px;
+  margin-bottom: 20px;
+  color: rgb(255, 255, 255);
+}
+
+.footer {
+  background-color: black;
+  height: 100px;
+  margin-top: 20px;
+  color: aliceblue;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-inline: 50px;
+  min-width: 300px;
+  height: 100vh;
+}
+</style>
---
+++ src/components/Note.vue
@@ -0,0 +1,47 @@
+<script setup>
+import fsx from "file-saver";
+function outFile() {
+  const txt = document.querySelector("#txtarea").value;
+
+  const blob = new Blob([txt], { type: "text/plain;charset=utf-8" });
+  fsx.saveAs(blob, "out.txt");
+
+  console.log(txt);
+}
+</script>
+
+<template>
+  <div class="tab">
+    <div @click="outFile()" class="btn">save text</div>
+  </div>
+  <textarea name="" id="txtarea" cols="30" rows="10"></textarea>
+</template>
+
+<style scoped>
+#txtarea {
+  padding: 30px;
+  font-size: 26px;
+  -webkit-box-shadow: 9px 8px 12px 1px rgba(0, 0, 0, 0.38);
+  box-shadow: 9px 8px 12px 1px rgba(0, 0, 0, 0.38);
+  resize: none;
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+}
+
+.tab {
+  width: auto;
+  margin: 10px;
+}
+.btn {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-content: center;
+  background-color: rgb(176, 84, 84);
+  height: auto;
+  border-radius: 10px;
+  padding: 10px;
+  cursor: pointer;
+}
+</style>
